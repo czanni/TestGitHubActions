17513d738dce7a48ace2930122d96a845e31a1d2
Cedric Zanni <cedric.zanni@loria.fr>
2023-11-02 13:01:19 +0100

    update project structure

--- test/Shape/TestShape.java
+++ test/Shape/TestShape.java
@@ -1,4 +1,6 @@
-package container;
+package Shape;
+
+import java.util.ArrayList;
 
 /*
 //import org.testng.annotations.Test;
@@ -14,10 +16,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Timeout.ThreadMode;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Set;
+
 import java.util.concurrent.TimeUnit;
 
 public class TestShape {
@@ -26,7 +25,21 @@ public class TestShape {
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS,  threadMode=Timeout.ThreadMode.SEPARATE_THREAD)    
     public void test_Empty() {
     	assertTrue(true);
-        assertEquals(0, 0);
     }
     
+    @Test
+    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS,  threadMode=Timeout.ThreadMode.SEPARATE_THREAD)    
+    public void test_Area() 
+    {
+      ArrayList<Shape> shapeList = new ArrayList<Shape>(5);
+      shapeList.add(new Square(2));
+      shapeList.add(new Square(4));
+      double sum = Application.sumArea(shapeList);
+      assertEquals(sum, 2.0*2.0+4.0*4.0); // warning float conversions
+    }
+    
+//        Shape test[] = new Shape[5];
+//        shapeList.toArray(test);
+//        System.out.println("Num greater : " + countAreaGreaterThan(test, 2.0));
+
 }
